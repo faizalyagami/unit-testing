6fb4e3ad62f3503ba1ccc589b4f7635c516e2012
faizalyagami <faizalyagami@gmail.com>
2023-11-03 12:41:45 +0700

    has coverage

--- _test_/gradeCalculation.test.js
+++ _test_/gradeCalculation.test.js
@@ -1,6 +1,21 @@
-const { averageExams } = require("../gradeCalculations");
+const { averageExams, isStudentPassExam } = require("../gradeCalculations");
 
-test("it should return exact average", () => {
+describe("grade calculations", () => {
+  test("it should return exact average", () => {
+    const listValueOfExams = [80, 100, 100, 80];
+    expect(averageExams(listValueOfExams)).toEqual(90);
+  });
+});
+
+/**
+ * integration testing
+ */
+test("it should return exam passed status", () => {
   const listValueOfExams = [80, 100, 100, 80];
-  expect(averageExams(listValueOfExams)).toEqual(90);
+  expect(isStudentPassExam(listValueOfExams, "faisal")).toEqual(true);
+});
+
+test("it should return exam failed status", () => {
+  const listValueOfExams = [50, 50, 70, 80];
+  expect(isStudentPassExam(listValueOfExams, "faisal")).toEqual(false);
 });
